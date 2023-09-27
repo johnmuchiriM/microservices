d5484dd6ebe67ffb3954a347f81d96f8f9ba8eab
John Muchiri <jmaina@cloudit.africa>
2023-09-27 11:17:33 +0300

    [main] tolerance through retry slf4j

--- currency-exchange-service/src/main/java/com/jeanfrancaise/microservices/currencyexchangeservice/controller/CircuitBreakerController.java
+++ currency-exchange-service/src/main/java/com/jeanfrancaise/microservices/currencyexchangeservice/controller/CircuitBreakerController.java
@@ -1,14 +1,29 @@
 package com.jeanfrancaise.microservices.currencyexchangeservice.controller;
 
 
+import io.github.resilience4j.retry.annotation.Retry;
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class CircuitBreakerController {
+
+    private Logger logger = LoggerFactory.getLogger(CircuitBreakerController.class);
     @GetMapping("/sample-api")
+    @Retry(name="sample-api", fallbackMethod = "hardcodedResponse")
     public String sampleApi(){
-        return "Sample API";
+        logger.info("Sample Api call received ");
+        ResponseEntity<String> forEntity = new RestTemplate().getForEntity("http://locahost:8080/some-dummy-url", String.class);
+        return forEntity.getBody();
+
+    }
+
+    public String hardcodedResponse(Exception ex){
+        return "fallback-response";
     }
 
 }
