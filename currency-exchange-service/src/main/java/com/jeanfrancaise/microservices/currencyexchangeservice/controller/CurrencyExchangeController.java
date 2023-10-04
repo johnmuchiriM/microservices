8e10253d2adff66f564af1314df37855d3ae39da
John Muchiri <jmaina@cloudit.africa>
2023-10-04 10:08:22 +0300

    [main] distributed tracing, sending info from currencyConversion to Zipkin

--- currency-exchange-service/src/main/java/com/jeanfrancaise/microservices/currencyexchangeservice/controller/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/com/jeanfrancaise/microservices/currencyexchangeservice/controller/CurrencyExchangeController.java
@@ -3,6 +3,8 @@ package com.jeanfrancaise.microservices.currencyexchangeservice.controller;
 
 import com.jeanfrancaise.microservices.currencyexchangeservice.entity.CurrencyExchange;
 import com.jeanfrancaise.microservices.currencyexchangeservice.repository.CurrencyExchangeRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CurrencyExchangeController {
+    private Logger logger = LoggerFactory.getLogger(CurrencyExchangeController.class);
 
     @Autowired
     private CurrencyExchangeRepository repository;
@@ -19,6 +22,8 @@ public class CurrencyExchangeController {
     private Environment environment;
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
     public CurrencyExchange retrieveExchangeValue(@PathVariable String from, @PathVariable String to){
+
+        logger.info("retrieveExchangeValue called with {} to {}",from,to);
         CurrencyExchange currencyExchange = repository.findByFromAndTo(from,to);
         if(currencyExchange == null){
             throw new RuntimeException("Unable to Find data for"+ from +"to"+ to);
